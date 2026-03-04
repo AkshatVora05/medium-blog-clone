9c3e78a22dcf41d378fd3d78b46601f95690780b
Akshat Vora <voraakshat05@gmail.com>
2026-03-04 17:54:12 +0530

    New Blog page UI change

--- frontend/src/components/CreateBlogSkeleton.tsx
+++ frontend/src/components/CreateBlogSkeleton.tsx
@@ -5,16 +5,12 @@ export const CreateBlogSkeleton = () => {
       <div className="flex flex-col mt-8">
         <div className="flex justify-center">
           <div className="max-w-[90vw] w-full">
-
             <div className="h-10 w-full bg-gray-200 rounded-lg"></div>
-
             <div className="mt-4 h-64 w-full bg-gray-200 rounded-lg"></div>
-
             <div className="mt-4 flex gap-3">
               <div className="h-10 w-40 bg-gray-200 rounded-lg"></div>
               <div className="h-10 w-40 bg-gray-200 rounded-lg"></div>
             </div>
-
           </div>
         </div>
       </div>
